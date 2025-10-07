5c4b49066b07efc5c7e6f2643ed1d70ce68f5f9f
Kamil <wegrzynk1@wp.pl>
2025-10-07 09:48:50 +0000

    c1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-(function () {
-  //TODO
+(function() {
+  var ex1_button = document.getElementById('ex1_button')
+
+  function Cw1() {
+    var count = "";
+    for (var i = 0; i < 10; i++) {
+      count += i;
+      count += ", ";
+    }
+    document.getElementById('ex1_content').innerHTML = `${count}`;
+  }
+
+  ex1_button.onclick = Cw1;
 })();
